564f2b62d2dea1e583128eeaf3fe9ada6c97539d
rstainsby <rstainsbydevelopment@gmail.com>
2025-01-28 13:09:45 +1000

    feat: add ThemeSelector component

---
+++ components/TheThemeSelector.vue
@@ -0,0 +1,142 @@
+<template>
+  <fieldset id="color-scheme" :class="$style['theme-selector']">
+    <div>
+      <input 
+        type="radio" 
+        id="system-theme" 
+        :class="[$style['no-show'], $style['theme-input'], $style['theme-input--system']]"
+        name="color-scheme" 
+        value="system" 
+        v-model="theme"
+        @change="setTheme(theme)">
+      <label for="system-theme" :class="$style['theme-label']">
+        <NuxtIcon name="icon-computer" :class="$style['theme-icon']" />
+      </label>
+    </div>
+
+    <div>
+      <input 
+        type="radio" 
+        id="light-theme" 
+        :class="[$style['no-show'], $style['theme-input'], $style['theme-input--light']]"
+        name="color-scheme" 
+        value="light"
+        v-model="theme" 
+        @change="setTheme(theme)">
+      <label for="light-theme" :class="$style['theme-label']">
+        <NuxtIcon name="icon-light-theme" :class="$style['theme-icon']"/>
+      </label>
+    </div>
+
+    <div>
+      <input 
+        type="radio" 
+        id="dark-theme" 
+        :class="[$style['no-show'], $style['theme-input'], $style['theme-input--dark']]"
+        name="color-scheme" 
+        value="dark" 
+        v-model="theme" 
+        @change="setTheme(theme)">
+      <label for="dark-theme" :class="$style['theme-label']">
+        <NuxtIcon name="icon-dark-theme" :class="$style['theme-icon']"/>
+      </label>
+    </div>
+  </fieldset>
+</template>
+
+<script setup lang="ts">
+const theme = ref(localStorage.getItem('theme') || 'system');
+
+const setTheme = (newTheme: string) => {
+  localStorage.setItem('theme', newTheme);
+  theme.value = newTheme;
+  document.documentElement.setAttribute('data-theme', newTheme);
+};
+</script>
+
+<style module>
+  .theme-selector {
+    --icon-size: 1.5rem;
+    container-type: size;
+    container-name: theme-selector;
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 4px 0;
+    height: calc(var(--icon-size) + 0.5rem);
+    width: calc((var(--icon-size) * 3) + 2.8rem);
+    padding: 0.25rem 0.75rem;
+    cursor: pointer;
+    box-shadow: inset 1px 1px 0px rgba(0, 0, 0, 0.3);
+    border-radius: 100px;
+    background-color: hsl(var(--cl-primary));
+    color: hsl(var(--cl-primary-text));
+    fill: hsl(var(--cl-primary-text));
+  }
+
+  .theme-label {
+    position: relative;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: var(--cl-bg);
+    border-radius: var(--border-radius);
+    width: max-content;
+    font-size: var(--fs-icons);
+    cursor: pointer;
+  }
+
+  .theme-icon {
+    z-index: 3;
+    background-color: transparent;
+  }
+
+  .theme-icon > svg {
+    width: var(--icon-size);
+    height: var(--icon-size);
+    margin-bottom: 0; /* Overwriting default style from nuxt-icons package */
+  }
+
+  .theme-input:checked + .theme-label {
+    color: hsl(var(--cl-primary));
+    fill: hsl(var(--cl-primary));
+  }
+
+  .theme-selector::after {
+    position: absolute;
+    content: "";
+    width: calc(var(--icon-size) + 0.75rem);
+    height: calc(var(--icon-size) + 0.25rem);
+    border-radius: 100px;
+    background-color: hsl(var(--cl-primary-text));
+    z-index: 2;
+    left: 0;
+    box-shadow: 1px 1px 0px rgba(0, 0, 0, 0.3);
+    transition: 0.2s linear;
+  }
+
+  .theme-selector:has(.theme-input--dark:checked).theme-selector::after {
+    transform: translateX(calc(100cqw - var(--icon-size) + 0.5rem));
+  }
+
+  .theme-selector:has(.theme-input--light:checked).theme-selector::after {
+    transform: translateX(calc(50cqw - (var(--icon-size) / 2) + 0.325rem));
+  }
+
+  .theme-selector:has(.theme-input--system:checked).theme-selector::after {
+    transform: translateX(0.35rem);
+  }
+
+  .no-show {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+  }
+</style>
