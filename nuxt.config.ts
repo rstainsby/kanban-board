75167c69bffb8b40ab5eadafbf55ebf4908c0ce7
rstainsby <rstainsbydevelopment@gmail.com>
2025-01-28 14:42:04 +1000

    chore: install and configure PrimeVue UI library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
+// import Theme from './styles/theme'
+import Theme from './styles/theme'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules:['@primevue/nuxt-module'],
+  primevue: {
+    options: {
+      theme: {
+        preset: Theme,
+        options: {
+          darkModeSelector: '[data-theme="dark"]',
+        }
+      }
+    }
+  }
 })
